f507cf2930f99b9cab9a4f7341928864d33f8c50
tusarxkumar <tusarkumarparida9@gmail.com>
2024-06-25 20:48:34 +0530

    first commit

---
+++ src/components/LeftNav.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import LeftNavMenuItem from "./LeftNavMenuItem";
+import { categories } from "../utils/constants";
+import { Context } from "../context/contextApi";
+
+const LeftNav = () => {
+    const { selectedCategory, setSelectedCategory, mobileMenu } = useContext(Context); 
+    return <div className='md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[240] md:translate-x-0 transition-all'>
+        <div className="flex px-5 flex-col">
+            {categories.map((item) => {
+                return (
+                    <React.Fragment>
+                        <LeftNavMenuItem
+                            text={item.type === "home" ? "Home" : item}
+                            icon={item.icon}
+                            action={() => { }}
+                            className={`${selectedCategory === item.name ? "bg-white/[0.15" : ""
+                                }`}
+                        />
+                        {item.divider && (
+                            <hr className="my-5 border-white/[0.2]" />
+                        )}
+                    </React.Fragment>
+                );
+            })}
+            <hr className="my-5 border-white/[0.2]" />
+            <div className="text-white/[0.5] text-[12px]">
+                Clone by Tusar Kumar Parida
+            </div>
+        </div>
+    </div>
+
+}
+
+export default LeftNav;
